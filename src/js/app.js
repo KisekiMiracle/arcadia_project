7013a9bead777f2db560fea873992542e9329554
KisekiMiracle <luisazaldegui99@gmail.com>
2025-02-11 01:54:18 -0500

    update: created header web component

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import { ArcadiaHeader } from "./components/common/_header.js";
+
+// define Web Components
+customElements.define("arcadia-header", ArcadiaHeader);
+
+// Navigation Router
---
+++ src/js/components/common/_header.js
@@ -0,0 +1,77 @@
+export class ArcadiaHeader extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  underlineActiveIndex(link) {
+    const currentPath = window.location.pathname.slice("1");
+
+    const hrefArray = link.href.split("/");
+    const thisPath = hrefArray[hrefArray.length - 1];
+
+    console.log(`Current Path: ${currentPath} || This Path: ${thisPath}`);
+
+    if (currentPath === thisPath) {
+      link.classList.add("active");
+    }
+  }
+
+  connectedCallback() {
+    // Create shadow root
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const nav = document.createElement("nav");
+    nav.setAttribute("class", "c-header");
+
+    const unorderedList = document.createElement("ul");
+
+    const logo = document.createElement("img");
+    logo.setAttribute("class", "c-header-logo");
+    logo.setAttribute("src", "/static/img/placeholder_logo.png");
+    logo.setAttribute("alt", "");
+
+    // Append Li to Lu
+    for (let i = 0; i <= 2; i++) {
+      const listItem = document.createElement("li");
+
+      switch (i) {
+        case 0: //logo
+          listItem.appendChild(logo);
+          unorderedList.appendChild(listItem);
+          break;
+        case 1: {
+          // Heroes
+          const link = document.createElement("a");
+          link.setAttribute("class", "c-header-link");
+          link.setAttribute("href", "/pages/heroes.html");
+          link.innerText = "Heroes";
+          listItem.appendChild(link);
+          unorderedList.appendChild(listItem);
+
+          this.underlineActiveIndex(link);
+          break;
+        }
+        case 2: {
+          // DevLog
+          const link = document.createElement("a");
+          link.setAttribute("class", "c-header-link");
+          link.innerText = "Dev Log";
+          listItem.appendChild(link);
+          unorderedList.appendChild(listItem);
+          break;
+        }
+      }
+    }
+    // Append Child to Nav
+    nav.appendChild(unorderedList);
+
+    // Apply scss styles
+    const styles = document.createElement("link");
+    styles.setAttribute("rel", "stylesheet");
+    styles.setAttribute("href", "/src/scss/dist/css/header.min.css");
+
+    // Attach elements to shadow dom
+    shadow.appendChild(styles);
+    shadow.appendChild(nav);
+  }
+}
