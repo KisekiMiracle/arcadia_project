f95e8bad14ea5e3ff31ba0dbba1fe8fa448d933b
KisekiMiracle <luisazaldegui99@gmail.com>
2025-02-11 04:18:37 -0500

    I'm tired

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import { ArcadiaHeader } from "./components/common/_header.js";
+import { ArcadiaHeader } from "./components/common/header.js";
 
 // define Web Components
 customElements.define("arcadia-header", ArcadiaHeader);
--- src/js/components/common/header.js
+++ src/js/components/common/header.js
@@ -37,7 +37,10 @@ export class ArcadiaHeader extends HTMLElement {
           // Home Page
           const link = document.createElement("a");
           link.setAttribute("class", "c-header-logo");
-          link.setAttribute("href", "./");
+          // Uncomment on Development
+          link.setAttribute("href", "./index.html");
+          // Uncomment on Production
+          //link.setAttribute("href", "./");
           link.appendChild(logo);
           listItem.appendChild(link);
           unorderedList.appendChild(listItem);
@@ -49,7 +52,10 @@ export class ArcadiaHeader extends HTMLElement {
           // Heroes Page
           const link = document.createElement("a");
           link.setAttribute("class", "c-header-link");
-          link.setAttribute("href", "./characters");
+          // Uncomment on Development
+          link.setAttribute("href", "./characters.html");
+          // Uncomment on Production
+          //link.setAttribute("href", "./characters");
           link.innerText = "CHARACTERS";
           listItem.appendChild(link);
           unorderedList.appendChild(listItem);
